b502cbb0767d7ca3242b822bb171c2cb596b244b
Jana Horáková <horakovajana2017@gamil.com>
2023-03-08 20:51:05 +0100

    HW

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 /*
 Místo pro váš program
 */
+/*Cviceni 1
+Vytvořte funkci salary se třemi parametry
+
+wage - hrubá mzda v korunách za hodinu
+hours - kolik hodin denně průměrně pracujete
+days - kolik dní v měsící průměrně pracujete
+Funkce spočítá vaši hrubou měsíční mzdu v celých korunách.
+
+Dále vytvořte funkci taxed, která na vstupu obdrží částku a procento zdanění, 
+a vrátí částku v celých korunách zdaněnou podle zadaných procent.
+
+Použítím funkcí salary a taxed spočítejte svoji měsíční mzdu po 15% zdanění.*/
+
+const salary = (wage, hours, days) => {
+    return wage * hours * days
+}
+
+//console.log(salary(5,5,5))
+
+const taxed = ()
